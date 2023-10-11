11e4ce1c47eaf405d465b5dea6c03d2d3d4002c3
Junior-Shyko <franciscoanto@gmail.com>
2023-10-10 23:59:05 -0300

    :tada: Iniciado backend #6
    Criado algumas classes e migration para contato

---
+++ app/Http/Controllers/PhoneController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePhoneRequest;
+use App\Http\Requests\UpdatePhoneRequest;
+use App\Models\Phone;
+
+class PhoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePhoneRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Phone $phone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Phone $phone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePhoneRequest $request, Phone $phone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Phone $phone)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_10_11_024132_create_phones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->string('number', 50)->nullable(false);
+            $table->string('object_id', 50)->nullable();
+            $table->string('object_type' , 150)->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phones');
+    }
+};
