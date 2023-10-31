90f2edb1e9e4a8ddb4ba6e2d271c3e69dfebc1f0
Junior-Shyko <franciscoanto@gmail.com>
2023-10-30 23:45:03 -0300

    Finalizado componente de veículo

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+use App\Http\Services\ProposalService;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vehicle $vehicle)
+    {
+        //
+    }
+
+    public function createOrUpdate(Request $request)
+    {
+        $commercial = new Vehicle();
+        ProposalService::createOrUpdate($commercial, $request);
+    }
+}
