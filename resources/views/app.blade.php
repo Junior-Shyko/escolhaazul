61de0d00b35604dec73758542e6908a968d0ef06
Junior-Shyko <franciscoanto@gmail.com>
2023-11-08 19:19:38 -0300

    Add meta theme-color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="theme-color" content="#2587E9" />
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
