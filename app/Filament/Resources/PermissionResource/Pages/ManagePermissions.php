365d2d62b8357566cce386ffa08a272cc9e9f5aa
Junior-Shyko <franciscoanto@gmail.com>
2023-11-24 00:21:24 -0300

    :construction: #20 - Permissoes de usuário

--- app/Filament/Resources/PermissionResource/Pages/ManagePermissions.php
+++ app/Filament/Resources/PermissionResource/Pages/ManagePermissions.php
@@ -9,6 +9,8 @@ use Filament\Resources\Pages\ManageRecords;
 class ManagePermissions extends ManageRecords
 {
     protected static string $resource = PermissionResource::class;
+    protected static ?string $title = 'Permissões';
+    protected ?string $subheading = 'Lista de permissões de uso dos usuários';
 
     protected function getHeaderActions(): array
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,6 @@ class User extends Authenticatable implements FilamentUser
     public function canAccessPanel(Panel $panel): bool
     {
         // return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
-        return true;
+        return $this->hasPermissionTo('access_admin');
     }
 }
