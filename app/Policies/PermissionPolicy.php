0260ff45b9cba4ef8eace4d634c130ffac9302ab
Junior-Shyko <franciscoanto@gmail.com>
2023-11-24 19:52:48 -0300

    :construction: Criado Policy de Role e Permission
    Criado, policy e registrado no authserviceprovider]

--- app/Policies/PermissionPolicy.php
+++ app/Policies/PermissionPolicy.php
@@ -2,25 +2,26 @@
 
 namespace App\Policies;
 
+use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class UserPolicy
+class PermissionPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        return $user->hasPermissionTo('user_read');
+        return $user->hasPermissionTo('permission_read');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model): bool
+    public function view(User $user, Permission $permission): bool
     {
-        return $user->hasPermissionTo('user_read');
+        //
     }
 
     /**
@@ -28,38 +29,29 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        return $user->hasPermissionTo('user_create');
+        return $user->hasPermissionTo('permission_read');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model): bool
+    public function update(User $user, Permission $permission): bool
     {
-        return $user->hasPermissionTo('user_update');
-    }
-
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function edit(User $user, User $model): bool
-    {
-        return $user->hasPermissionTo('access_admin');
+        return $user->hasPermissionTo('permission_read');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, User $model): bool
+    public function delete(User $user, Permission $permission): bool
     {
-        return $user->hasPermissionTo('user_delete');
+        return $user->hasPermissionTo('permission_read');
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, User $model): bool
+    public function restore(User $user, Permission $permission): bool
     {
         //
     }
@@ -67,7 +59,7 @@ class UserPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, User $model): bool
+    public function forceDelete(User $user, Permission $permission): bool
     {
         //
     }
--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -2,25 +2,26 @@
 
 namespace App\Policies;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class UserPolicy
+class RolePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        return $user->hasPermissionTo('user_read');
+        return $user->hasPermissionTo('role_read');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model): bool
+    public function view(User $user, Role $role): bool
     {
-        return $user->hasPermissionTo('user_read');
+        return $user->hasPermissionTo('role_read');
     }
 
     /**
@@ -28,38 +29,29 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        return $user->hasPermissionTo('user_create');
+        return $user->hasPermissionTo('role_read');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model): bool
+    public function update(User $user, Role $role): bool
     {
-        return $user->hasPermissionTo('user_update');
-    }
-
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function edit(User $user, User $model): bool
-    {
-        return $user->hasPermissionTo('access_admin');
+        return $user->hasPermissionTo('role_read');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, User $model): bool
+    public function delete(User $user, Role $role): bool
     {
-        return $user->hasPermissionTo('user_delete');
+        return $user->hasPermissionTo('role_read');
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, User $model): bool
+    public function restore(User $user, Role $role): bool
     {
         //
     }
@@ -67,7 +59,7 @@ class UserPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, User $model): bool
+    public function forceDelete(User $user, Role $role): bool
     {
         //
     }
