eef04a31399709c046388684c011355696f951fc
Junior-Shyko <franciscoanto@gmail.com>
2023-11-25 16:02:20 -0300

    :sparkles: #21 - Criado pdf com dados da proposta
    Instalado dompdf, carbon, refatorado funcoes e gerando o pdf

--- app/Models/RentalData.php
+++ app/Models/RentalData.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RentalData extends Model
 {
@@ -38,6 +39,10 @@ class RentalData extends Model
         return $this->belongsTo(Term::class);
     }
 
-    
+    public function bank(): HasMany
+    {
+        return $this->hasMany(Bank::class, 'object_id');
+    }
+
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Filament\Panel;
+use App\Models\Personal;
 use App\Models\Property;
 use App\Models\DataPersonal;
 use Laravel\Sanctum\HasApiTokens;
@@ -70,4 +71,35 @@ class User extends Authenticatable implements FilamentUser
     {
         return $this->hasMany(RealState::class, 'object_id');
     }
+
+    //Relação com referencia pessoal
+    public function referencePersonal(): HasMany
+    {
+        return $this->hasMany(Personal::class, 'object_id');
+    }
+
+    //Relação com os arquivos de usuário
+    public function files(): HasMany
+    {
+        return $this->hasMany(File::class, 'object_id');
+    }
+  
+    // Relação com referencia comercial
+    public function commercial(): HasMany
+    {
+        return $this->hasMany(Commercial::class, 'object_id');
+    }
+    
+    //Relação com referencia bancaria
+    public function bank(): HasMany
+    {
+        return $this->hasMany(Bank::class, 'object_id');
+    }
+        
+    //Relação com referencia bancaria
+    public function address(): HasMany
+    {
+        return $this->hasMany(Address::class, 'object_id');
+    }
+
 }
