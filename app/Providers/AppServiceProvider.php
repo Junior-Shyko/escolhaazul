8344ea95ef1ab09a2b045dd7d4b36a31564dcd40
Junior-Shyko <franciscoanto@gmail.com>
2023-11-24 21:38:13 -0300

    :hammer: Finalizado papeis e permissão

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentColor::register([
+            'danger' => Color::Red,
+            'gray' => Color::Zinc,
+            'info' => Color::Blue,
+            'primary' => Color::hex('#2587E9'),
+            'success' => Color::Green,
+            'warning' => Color::Amber,
+        ]);
     }
 }
