5db44f5cb36ce4014c95e594333a4bfbe8469201
Junior-Shyko <franciscoanto@gmail.com>
2023-12-05 00:02:20 -0300

    Mudança de titulo de edição

--- app/Filament/Resources/ProfessionalResource/Pages/EditProfessional.php
+++ app/Filament/Resources/ProfessionalResource/Pages/EditProfessional.php
@@ -9,7 +9,8 @@ use Filament\Resources\Pages\EditRecord;
 class EditProfessional extends EditRecord
 {
     protected static string $resource = ProfessionalResource::class;
-
+    protected static ?string $title = 'Edição';
+    protected ?string $subheading = 'Edite seus dados profissionais';
     protected function getHeaderActions(): array
     {
         return [
