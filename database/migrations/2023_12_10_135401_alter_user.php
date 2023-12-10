5845496b6dab93a36af2c294475d1ec24f142000
Junior-Shyko <franciscoanto@gmail.com>
2023-12-10 11:30:22 -0300

    #23 - Alterdo migrations para exclusao de usuario

---
+++ database/migrations/2023_12_10_135401_alter_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('data_personals', function (Blueprint $table) {
+            
+            $table->dropForeign('data_personals_user_id_foreign');
+            $table->foreign('user_id')
+            ->references('id')->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
