090c2bae6544a0a8fc77401aa3cf4b66385b11e2
Junior-Shyko <franciscoanto@gmail.com>
2024-01-08 01:18:21 -0300

    :construction: Criado pagina customizada dos arquivos
    Criado pagina pelo filament e usando livewire

---
+++ app/Filament/Pages/File.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class File extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationGroup = 'Dados';
+    protected static ?string $navigationLabel = 'Arquivos';
+
+    protected static ?string $title = 'Arquivos';
+    protected ?string $subheading = 'Todos os arquivos da proposta';
+
+
+
+    protected static string $view = 'filament.pages.file';
+}
