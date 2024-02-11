85abb858a396b0340114f3ef9add4f936ed1c6bf
Junior-Shyko <franciscoanto@gmail.com>
2024-02-11 17:10:35 -0300

    :construction: Criado seed de endereço

---
+++ database/seeders/AddressSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AddressSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create('pt_BR');
+        $address = [];
+        $type = $faker->randomElements(['address_personal','professional','professional','property']);
+        for ($i = 0; $i < 200; $i++) {
+            $userId = $faker->numberBetween(1, 242);
+            $address['cep'] = $faker->randomNumber(8, true);
+            $address['address'] = $faker->streetName();
+            $address['number'] = $faker->randomNumber();
+            $address['complement'] = $faker->stateAbbr();
+            $address['neighborhood'] = $faker->streetName() ;
+            $address['city'] =$faker->city() ;
+            $address['UF'] = $faker->cityPrefix();
+            $address['object_id'] = $userId;
+            $address['object_type'] = $type[0];
+            $address['user_id'] = $userId;
+            \App\Models\Address::create($address);
+        }
+    }
+}
