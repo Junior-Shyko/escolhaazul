d133cbb852e5eea79f978f91480d305262686a7c
Junior-Shyko <franciscoanto@gmail.com>
2024-02-13 15:05:07 -0300

    :rocket: Adicionado nginx ao projeto

--- app/Filament/Widgets/FeedbackOverview.php
+++ app/Filament/Widgets/FeedbackOverview.php
@@ -16,27 +16,33 @@ class FeedbackOverview extends BaseWidget
         $rentalPF = RentalData::where('typeRentalUser', 'Pessoa Física')->count();
         $rentalPJ = RentalData::where('typeRentalUser', 'Pessoa Jurídica')->count();
         $incomplet = RentalData::where('status', 'incompleta')->count();
-        return [
-            Stat::make('Usuários', $user)
-                ->description('Total de usuários')
-                ->descriptionIcon('heroicon-m-user-group')
-                ->color(Color::hex('#0c498c')),
 
-            Stat::make('Proposta PF', $rentalPF)
-                ->description('Total Prop. Pessoa Física')
-                ->descriptionIcon('heroicon-s-document-duplicate')
-                ->color(Color::hex('#0c4b8c')),
+        if(!auth()->user()->hasRole('common')) {
+            return [
+                Stat::make('Usuários', $user)
+                    ->description('Total de usuários')
+                    ->descriptionIcon('heroicon-m-user-group')
+                    ->color(Color::hex('#0c498c')),
+    
+                Stat::make('Proposta PF', $rentalPF)
+                    ->description('Total Prop. Pessoa Física')
+                    ->descriptionIcon('heroicon-s-document-duplicate')
+                    ->color(Color::hex('#0c4b8c')),
+    
+                Stat::make('Proposta PJ', $rentalPJ)
+                    ->description('Total Pro. Pessoa Jurídica')
+                    ->descriptionIcon('heroicon-m-document-duplicate')
+                    ->color(Color::hex('#2484ec')),
+                Stat::make('Propostas Incompletas', $incomplet)
+                    ->description('Total de prop. incompletas')
+                    ->descriptionIcon('heroicon-s-arrow-up-on-square-stack')
+                    ->color(Color::hex('#86bcf4')),
+    
+            ];
+        }
 
-            Stat::make('Proposta PJ', $rentalPJ)
-                ->description('Total Pro. Pessoa Jurídica')
-                ->descriptionIcon('heroicon-m-document-duplicate')
-                ->color(Color::hex('#2484ec')),
-            Stat::make('Propostas Incompletas', $incomplet)
-                ->description('Total de prop. incompletas')
-                ->descriptionIcon('heroicon-s-arrow-up-on-square-stack')
-                ->color(Color::hex('#86bcf4')),
-
-        ];
+        return [];
+       
     }
 
 }
