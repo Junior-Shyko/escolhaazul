525ceaef0fdeaf677d0015318ddeb0c4ade2ff41
Junior-Shyko <franciscoanto@gmail.com>
2024-02-25 20:29:18 -0300

    Git action e acessando servidor

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+echo "Subindo docker"
+docker-compose up -d
+echo "Atualizando Dependências"
 docker exec ea composer install
 docker exec ea composer update
+echo "Php Artisan"
 docker exec ea php artisan cache:clear
 docker exec ea php artisan view:clear
 docker exec ea php artisan artisan migrate
