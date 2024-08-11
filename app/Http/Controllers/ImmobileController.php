26e11c18ec1dbcf9a8c214676f75e971953d4c1f
Junior-Shyko <franciscoanto@gmail.com>
2024-08-11 19:44:59 -0300

    :construction: #54 - Iniciando classes de imoveis

---
+++ app/Http/Controllers/ImmobileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Immobile;
+use Illuminate\Http\Request;
+
+class ImmobileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Immobile $immobile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Immobile $immobile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Immobile $immobile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Immobile $immobile)
+    {
+        //
+    }
+}
