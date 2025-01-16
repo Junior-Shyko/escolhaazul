7c88da2f57dfbe6dba6eb14c9cec7af17887c19f
Junior-Shyko <franciscoanto@gmail.com>
2025-01-16 11:38:55 -0300

    Add rota proposal

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('form')->group( function () {
-    // Route::post('/proposal', [ProposalController::class, 'createUser'])->name('form/proposal');
+    Route::post('/proposal', [ProposalController::class, 'createUser'])->name('form/proposal');
     Route::post('address', [AddressController::class, 'store'] )->name('api/form/address');
     Route::put('address',  [AddressController::class, 'update'] )->name('api/form/address/update');
     Route::post('upload/proposal/{id}/{type}', [FileController::class, 'store'])->name('upload');
